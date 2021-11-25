0c2e621aa99ec70473ba919385a861e09b4b84bc
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2021-11-25 21:09:13 +0100

    Fix item factory to pass tests

--- src/main/java/com/gildedrose/model/ItemFactory.java
+++ src/main/java/com/gildedrose/model/ItemFactory.java
@@ -1,16 +1,27 @@
 package com.gildedrose.model;
 
+import com.gildedrose.QualityLimitException;
+
 public class ItemFactory {
 
 	public static Item normalItem(String name, int sellIn, int quality) {
+		if (quality > 50) {
+			throw new QualityLimitException("Quality for item cannot exceed 50.");
+		}
 		return new NormalItem(name, sellIn, quality);
 	}
 
 	public static Item agedBrieItem(int sellIn, int quality) {
+		if (quality > 50) {
+			throw new QualityLimitException("Quality for item cannot exceed 50.");
+		}
 		return new AgedBrieItem("Aged Brie", sellIn, quality);
 	}
 
 	public static Item backstagePassesItem(int sellIn, int quality) {
+		if (quality > 50) {
+			throw new QualityLimitException("Quality for item cannot exceed 50.");
+		}
 		return new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
 	}
 
