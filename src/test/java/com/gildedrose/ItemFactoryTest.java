ed9cb592fbd72b9b33dc9073aa1fe734e5fb123c
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2021-11-25 21:57:43 +0100

    Add item factory tests

--- src/test/java/com/gildedrose/ItemFactoryTest.java
+++ src/test/java/com/gildedrose/ItemFactoryTest.java
@@ -11,6 +11,12 @@ public class ItemFactoryTest {
 	@Test
 	public void normalItemQualityCanNotExceed50WhenCreated() {
 		assertThrows(QualityLimitException.class, () -> ItemFactory.normalItem("item 1", 10, 51));
+
+		try {
+			ItemFactory.normalItem("item 1", 10, 51);
+		} catch (QualityLimitException ex) {
+			System.out.println(ex.getLocalizedMessage());
+		}
 	}
 
 	@Test
@@ -21,6 +27,28 @@ public class ItemFactoryTest {
 	@Test
 	public void agedBrieItemQualityCanNotExceed50WhenCreated() {
 		assertThrows(QualityLimitException.class, () -> ItemFactory.agedBrieItem(10, 51));
+	}
+	
+	@Test
+	public void normalItemQualityCanNotBeNegativeWhenCreated() {
+		assertThrows(QualityLimitException.class, () -> ItemFactory.normalItem("item 1", 10, -1));
+
+		try {
+			ItemFactory.normalItem("item 1", 10, -1);
+		} catch (QualityLimitException ex) {
+			System.out.println(ex.getLocalizedMessage());
+		}
+	}
+
+	@Test
+	public void backstagePassesItemQualityCanNotBeNegativeWhenCreated() {
+		assertThrows(QualityLimitException.class, () -> ItemFactory.backstagePassesItem(10, -1));
+	}
+
+	@Test
+	public void agedBrieItemQualityCanNotBeNegativeWhenCreated() {
+		assertThrows(QualityLimitException.class, () -> ItemFactory.agedBrieItem(10, -1));
+
 	}
 
 }
