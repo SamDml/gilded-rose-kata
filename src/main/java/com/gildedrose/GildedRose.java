d31336b09c7110e6b4f9babcdcf94867d37a12ef
Sam Demeulemeester <sam.demeulemeester@unifiedpost.com>
2021-11-25 12:16:43 +0100

    Restructure update quality logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,75 +4,70 @@ import java.util.Arrays;
 import java.util.List;
 
 class GildedRose {
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if ( ! isSpecialItem(items[i])) {
-            		if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-            }
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			if (!isSpecialItem(items[i])) {
+				if (items[i].quality > 0) {
+					items[i].quality = items[i].quality - 1;
+				}
+			}
 
-            if (items[i].name.equals("Aged Brie")
-                    || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+			if (items[i].name.equals("Aged Brie")
+					|| items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+					if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+						if (items[i].sellIn < 11) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+						if (items[i].sellIn < 6) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
+					}
+				}
+			}
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+				items[i].sellIn = items[i].sellIn - 1;
+			}
 
-            if (items[i].sellIn < 0) {
+			if (items[i].sellIn < 0) {
 
-            	if (items[i].name.equals("Aged Brie")) {
-            		if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-            	} else {
+				if (items[i].name.equals("Aged Brie")) {
+					if (items[i].quality < 50) {
+						items[i].quality = items[i].quality + 1;
+					}
+				} else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					items[i].quality = items[i].quality - items[i].quality;
 
-            		 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                         items[i].quality = items[i].quality - items[i].quality;
-                     } else {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+				} else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+					if (items[i].quality > 0) {
 
-    private boolean isSpecialItem(Item item) {
+						items[i].quality = items[i].quality - 1;
+					}
+				}
+			}
+		}
+	}
 
-    	List<String> specialItemNames = Arrays.asList(new String[]{
-    			"Aged Brie", 
-    			"Backstage passes to a TAFKAL80ETC concert", 
-    			"Sulfuras, Hand of Ragnaros"});
+	private boolean isSpecialItem(Item item) {
 
-    	return specialItemNames.contains(item.name);
+		List<String> specialItemNames = Arrays.asList(new String[] { "Aged Brie",
+				"Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" });
 
-    }
+		return specialItemNames.contains(item.name);
+
+	}
 }
