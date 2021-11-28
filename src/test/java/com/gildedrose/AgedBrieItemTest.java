899f695e3d84a1a5cf6edbccfed945caf219630a
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2021-11-28 11:52:47 +0100

    Split item test

---
+++ src/test/java/com/gildedrose/AgedBrieItemTest.java
@@ -0,0 +1,66 @@
+package com.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.gildedrose.model.Item;
+import com.gildedrose.model.ItemFactory;
+
+public class AgedBrieItemTest {
+
+	@Test
+	public void agedBrieItemItemNameReturnsItemName() {
+
+		Item item = ItemFactory.agedBrieItem(10, 10);
+
+		String returnedItemName = item.getName();
+
+		assertTrue(returnedItemName instanceof String);
+	}
+
+	@Test
+	public void agedBrieItemItemSellinDecreasesBy1AfterUpdateQuality() {
+
+		Item item = ItemFactory.agedBrieItem(10, 10);
+
+		item.updateQuality();
+
+		assertEquals(9, item.getSellIn());
+	}
+
+	@Test
+	public void agedBrieItemItemQualityIncreasesByOneOnUpdateQualityBelow50() {
+
+		Item item;
+		for (int initialQuality = 0; initialQuality < 50; initialQuality++) {
+			item = ItemFactory.agedBrieItem(10, initialQuality);
+
+			item.updateQuality();
+
+			assertEquals(initialQuality + 1, item.getQuality());
+		}
+	}
+
+	@Test
+	public void agedBrieItemItemQualityDoesNotExceed50AfterUpdateQuality() {
+
+		Item item = ItemFactory.agedBrieItem(10, 50);
+
+		item.updateQuality();
+
+		assertEquals(50, item.getQuality());
+	}
+
+	@Test
+	public void agedBrieItemItemQualityOnlyIncreasesByOneWhenUpdateQualityOneBelow50() {
+
+		Item item = ItemFactory.agedBrieItem(10, 49);
+
+		item.updateQuality();
+
+		assertEquals(50, item.getQuality());
+	}
+
+}
---
+++ src/test/java/com/gildedrose/BackstagePassesItemTest.java
@@ -0,0 +1,75 @@
+package com.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.gildedrose.model.Item;
+import com.gildedrose.model.ItemFactory;
+
+public class BackstagePassesItemTest {
+
+	@Test
+	public void backstagePassesItemItemNameReturnsItemName() {
+
+		Item item = ItemFactory.backstagePassesItem(10, 10);
+
+		String returnedItemName = item.getName();
+
+		assertTrue(returnedItemName instanceof String);
+	}
+
+	@Test
+	public void backstagePassesItemItemSellinDecreasesBy1AfterUpdateQuality() {
+
+		Item item = ItemFactory.backstagePassesItem(10, 10);
+
+		item.updateQuality();
+
+		assertEquals(9, item.getSellIn());
+	}
+
+	@Test
+	public void backstagePassesItemItemQualityIncreasesBy2WhenSellInIs10OrLessAndAbove5() {
+
+		Item item;
+		int oldQuality;
+		for (int i = 10; i > 5; i--) {
+			item = ItemFactory.backstagePassesItem(i, 10);
+			oldQuality= item.getQuality();
+
+			item.updateQuality();
+
+			assertEquals(oldQuality + 2, item.getQuality());
+			oldQuality = item.getQuality();
+		}
+	}
+
+	@Test
+	public void backstagePassesItemItemQualityIncreasesBy3WhenSellInIs5OrLessAndAboveZero() {
+
+		Item item;
+		int oldQuality;
+		for (int i = 5; i > 0; i--) {
+			item = ItemFactory.backstagePassesItem(i, 10);
+			oldQuality= item.getQuality();
+
+			item.updateQuality();
+
+			assertEquals(oldQuality + 3, item.getQuality());
+			oldQuality = item.getQuality();
+		}
+	}
+
+	@Test
+	public void backstagePassesItemItemQualityDropsToZeroAfterSellingDate() {
+
+		Item item = ItemFactory.backstagePassesItem(0, 25);
+
+		item.updateQuality();
+
+		assertEquals(0, item.getQuality());
+	}
+
+}
